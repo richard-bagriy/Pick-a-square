eba882f301566542f974c051e6b88550bee68db3
Richard <richard.bagriy@gmai.com>
2020-06-02 21:00:45 +0300

    Updates

---
+++ precache-manifest.8fe05166acd16c2ea55c207d4114a40d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f90a5e54016dc40a81614c4b780a035",
+    "url": "/Pick-a-square/index.html"
+  },
+  {
+    "revision": "8f8455550c177ce0df06",
+    "url": "/Pick-a-square/static/css/main.6e9d27ad.chunk.css"
+  },
+  {
+    "revision": "0f4b1d88f80a3519bc93",
+    "url": "/Pick-a-square/static/js/2.c3841c57.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/Pick-a-square/static/js/2.c3841c57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f8455550c177ce0df06",
+    "url": "/Pick-a-square/static/js/main.b11cac22.chunk.js"
+  },
+  {
+    "revision": "0b9cf4ca930b93187e81",
+    "url": "/Pick-a-square/static/js/runtime-main.d013cbdd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Pick-a-square/static/media/logo.5d5d9eef.svg"
+  }
+]);
